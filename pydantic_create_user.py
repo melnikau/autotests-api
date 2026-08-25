cfbb03629de2a2440474cf0d4776a84226bc92b0
melnikau <a.melnikau@gmail.com>
2026-08-26 00:16:02 +0300

    Знакомство с Pydantic. Практика написания Pydantic-моделей

---
+++ pydantic_create_user.py
@@ -0,0 +1,36 @@
+import uuid
+from tools.fakers import get_random_email
+from pydantic import BaseModel, Field, EmailStr, ValidationError, constr
+
+
+class UserSchema(BaseModel):
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+
+class CreateUserRequestSchema(BaseModel):
+    email: EmailStr = get_random_email()
+    password: constr(min_length=8)
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+
+class CreateUserResponseSchema(BaseModel):
+    user: UserSchema
+
+
+try:
+    user = CreateUserRequestSchema(
+        # email="user@example.com",
+        password="Pass1234",
+        lastName="Doe",
+        firstName="John",
+        middleName="Doe"
+    )
+    print(user)
+except ValidationError as error:
+    print(error)
