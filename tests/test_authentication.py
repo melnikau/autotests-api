513e6788704502128dbbc26e81b17ce789a38099
melnikau <a.melnikau@gmail.com>
2026-09-04 13:30:45 +0300

    Написание API автотестов используя Pytest. Практика написания API автотестов

---
+++ tests/test_authentication.py
@@ -0,0 +1,36 @@
+from http import HTTPStatus
+
+from clients.authentication.authentication_client import get_authentication_client
+from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema
+from tools.assertions.authentication import assert_login_response
+from tools.assertions.base import assert_status_code
+
+
+def test_login() -> None:
+    """
+    Проверяет успешную аутентификацию пользователя.
+    """
+
+    public_users_client = get_public_users_client()
+    authentication_client = get_authentication_client()
+
+    create_user_request = CreateUserRequestSchema()
+    create_user_response = public_users_client.create_user(create_user_request)
+    print('Create user data: ', create_user_response)
+
+    login_request = LoginRequestSchema(
+        email=create_user_request.email,
+        password=create_user_request.password,
+    )
+
+    print('Login request: ', login_request)
+
+    login_response = authentication_client.login_api(login_request)
+    login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
+
+    assert_status_code(login_response.status_code, HTTPStatus.OK)
+    assert_login_response(login_response_data)
+
+    LoginResponseSchema.model_validate_json(login_response.text)
